ed47826c50df64e50417f190b5dccf75b70e0286
scp-koakutsu <koakutsu@scala-partners.com>
2024-08-26 10:47:17 +0900

    ユーザー、水槽のリスト表示

--- src/rakuraku_apps/views/manage.py
+++ src/rakuraku_apps/views/manage.py
@@ -1,13 +1,32 @@
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from rakuraku_apps.models import TankModel, User
+from rakuraku_apps.forms.manage import UserForm
+from rakuraku_apps.forms.manage import TankForm
+
 
 class ManageView(TemplateView):
     template_name = 'manage/manage.html'
 
-class ManageUserView(TemplateView):
+class ManageUserView(ListView):
+    model = User
     template_name = 'manage/user.html'
+    context_object_name = 'users'
 
-class ManageTankView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = UserForm()
+        return context
+
+class ManageTankView(ListView):
+    model = TankModel
     template_name = 'manage/tank.html'
+    context_object_name = 'tanks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = TankForm()
+        return context
 
 class ManagValueView(TemplateView):
     template_name = 'manage/value.html'
